6bf4bfe767522623f748e74cfd0c140cc45636c4
Guilherme <guicomercial328@gmail.com>
2025-09-10 22:47:52 -0300

    Fazendo commit do repository/ClientRepository

--- src/main/java/com.xp.guardian/repository/ClientRepository.java
+++ src/main/java/com.xp.guardian/repository/ClientRepository.java
@@ -1,4 +1,7 @@
 package com.xp.guardian.repository;
 
-public class ClientRepository {
+import com.xp.guardian.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
