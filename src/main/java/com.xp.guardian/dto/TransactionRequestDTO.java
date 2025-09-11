a774b43736290b7fbb4c284b667194f7a39fa835
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-11 11:02:50 -0300

    Fazendo commit do dto/TransactionRequest e Response

--- src/main/java/com.xp.guardian/dto/TransactionRequestDTO.java
+++ src/main/java/com.xp.guardian/dto/TransactionRequestDTO.java
@@ -1,4 +1,13 @@
 package com.xp.guardian.dto;
 
-public class TransactionRequestDTO {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record TransactionRequestDTO(
+        @NotNull Long clientId,
+        @NotNull @Positive BigDecimal amount,
+        @NotBlank String description
+) {}
--- src/main/java/com.xp.guardian/dto/TransactionResponseDTO.java
+++ src/main/java/com.xp.guardian/dto/TransactionResponseDTO.java
@@ -1,4 +1,17 @@
 package com.xp.guardian.dto;
 
-public class TransactionResponseDTO {
-}
+import com.xp.guardian.enums.TransactionStatus;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record TransactionResponseDTO(
+        Long transactionId,
+        Long clientId,
+        BigDecimal amount,
+        String description,
+        TransactionStatus status,
+        Instant timestamp,
+        String message,
+        InvestmentSuggestionDTO investmentSuggestion
+) {}
