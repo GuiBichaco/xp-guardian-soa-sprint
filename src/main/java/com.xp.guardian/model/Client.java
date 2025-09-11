94afb8489f0e15867cd415366b91c31768ddbd30
Guilherme <guicomercial328@gmail.com>
2025-09-10 22:42:03 -0300

    Fazendo commit do model/Client

--- src/main/java/com.xp.guardian/model/Client.java
+++ src/main/java/com.xp.guardian/model/Client.java
@@ -1,4 +1,29 @@
 package com.xp.guardian.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private BigDecimal balance;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Transaction> transactions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<InvestmentSuggestion> investmentSuggestions = new ArrayList<>();
 }
