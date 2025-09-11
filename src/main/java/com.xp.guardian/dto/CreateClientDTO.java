51be3f2de88b554cb7fb25bc7531ea8f7c94db86
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-11 10:59:56 -0300

    Fazendo commit do dto/CreateClientDTO

--- src/main/java/com.xp.guardian/dto/CreateClientDTO.java
+++ src/main/java/com.xp.guardian/dto/CreateClientDTO.java
@@ -1,4 +1,14 @@
 package com.xp.guardian.dto;
 
-public class CreateClientDTO {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+
+public record CreateClientDTO(
+        @NotBlank String name,
+        @Email String email,
+        @NotNull @PositiveOrZero BigDecimal initialBalance
+) {}
